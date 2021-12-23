23da9cc50e26e96e8bd52ea6d04a02ddb239d85e
usman <usman.mushtaq848@gmail.com>
2021-12-23 22:32:33 +0500

    first completed

---
+++ app/src/main/java/bayie/alhalib/shop/helper/VolleyCallback.java
@@ -0,0 +1,7 @@
+package bayie.alhalib.shop.helper;
+
+public interface VolleyCallback {
+    void onSuccess(boolean result, String message);
+    //void onSuccessWithMsg(boolean result, String message);
+
+}
